0016094101758ee28fb7e75d6b5e4d4b0633947b
majidghassemi <mjghcr@gmail.com>
2024-07-22 20:02:16 -0400

    Modifications to mutation, selection based on tournament selection, and dynamic elitism added

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -12,31 +12,51 @@ class GeneticAlgorithm:
         self.rest_period = rest_period
         self.shift_lengths = [119] * self.shifts
         self.distance_matrix = self.generate_distance_matrix()
+        self.mutation_rate = 0.1
+        self.elite_size = int(0.05 * self.population_size)
+        self.tournament_size = 5  # Size of the tournament for parent selection
         self.population = self.initialize_population()
-        self.mutation_rate = 0.1 
 
     def generate_distance_matrix(self):
         return [[random.randint(10, 20) for _ in range(102)] for _ in range(102)]
 
     def initialize_population(self):
-        return [{shift: {vehicle: self.create_feasible_route(shift) for vehicle in range(self.vehicles)} for shift in range(self.shifts)} for _ in range(self.population_size)]
+        population = []
+        for _ in range(self.population_size):
+            individual = {}
+            for shift in range(self.shifts):
+                shift_routes = {vehicle: [] for vehicle in range(self.vehicles)}
+                individual[shift] = {vehicle: self.create_feasible_route(shift, shift_routes) for vehicle in range(self.vehicles)}
+                for vehicle in range(self.vehicles):
+                    shift_routes[vehicle] = individual[shift][vehicle][1]  # Update the shift routes with timings
+            population.append(individual)
+        return population
 
-    def create_feasible_route(self, shift):
+    def create_feasible_route(self, shift, shift_routes):
         route = [0]
         current_location = 0
         current_time = 0
         route_timings = [(current_location, current_time)]
+        visited_times = {0: current_time}  # Track the times each location was visited
 
         while current_time < self.shift_lengths[shift]:
             next_location = random.randint(1, 100)
             travel_time = self.distance_matrix[current_location][next_location]
             arrival_time = current_time + travel_time + self.patrol_time
 
+            # Check if next_location is being visited by another vehicle at the same time
+            if any(arrival_time == other_time for other_route in shift_routes.values() for loc, other_time in other_route if loc == next_location):
+                continue  # Skip this location if another vehicle is patrolling it at the same time
+
+            if next_location in visited_times and arrival_time - visited_times[next_location] < 30:
+                continue  # Skip this location if it was visited less than 30 minutes ago
+
             if arrival_time + self.distance_matrix[next_location][100] > self.shift_lengths[shift]:
                 break
 
             route.append(next_location)
             route_timings.append((next_location, arrival_time))
+            visited_times[next_location] = arrival_time
             current_location = next_location
             current_time = arrival_time
 
@@ -49,11 +69,15 @@ class GeneticAlgorithm:
 
     def evaluate_fitness(self, individual):
         unique_locations = set()
+        total_distance = 0
         for shift in individual:
             for vehicle in individual[shift]:
                 route, _ = individual[shift][vehicle]
                 unique_locations.update(route)
-        return len(unique_locations)
+                for i in range(len(route) - 1):
+                    total_distance += self.distance_matrix[route[i]][route[i+1]]
+        fitness = len(unique_locations) - (total_distance / 1000)
+        return fitness
 
     def crossover(self, parent1, parent2):
         child = {shift: {vehicle: None for vehicle in range(self.vehicles)} for shift in range(self.shifts)}
@@ -75,17 +99,25 @@ class GeneticAlgorithm:
                         route[idx1], route[idx2] = route[idx2], route[idx1]
 
     def select_parents(self):
-        parents = random.sample(self.population, 2)
-        return parents[0], parents[1]
+        return self.tournament_selection(), self.tournament_selection()
+
+    def tournament_selection(self):
+        tournament = random.sample(self.population, self.tournament_size)
+        fittest = max(tournament, key=self.evaluate_fitness)
+        return fittest
 
     def run(self):
         best_fitnesses = []
         best_solution = None
-        best_fitness = 0
+        best_fitness = float('-inf')
 
         for generation in range(self.generations):
             new_population = []
-            for _ in range(self.population_size // 2):
+            # Elitism: Preserve the best individuals
+            elite_individuals = sorted(self.population, key=self.evaluate_fitness, reverse=True)[:self.elite_size]
+            new_population.extend(elite_individuals)
+
+            for _ in range((self.population_size - self.elite_size) // 2):
                 parent1, parent2 = self.select_parents()
                 child1 = self.crossover(parent1, parent2)
                 child2 = self.crossover(parent2, parent1)
@@ -111,7 +143,7 @@ best_overall_solution = None
 best_overall_fitness = 0
 best_fitness_per_iteration = []
 average_fitness_per_iteration = []
-max_fitness_so_far = 0
+max_fitness_so_far = float('-inf')
 
 for i in range(100):
     ga = GeneticAlgorithm(vehicles=5, locations=99, shifts=4)
@@ -143,7 +175,3 @@ for shift in best_overall_solution:
         print("  Timing:")
         for loc, time in timings:
             print(f"    Location {loc} at time {time} minutes")
-
-
-
-# Genetic Dynamic VPS (GDVPS)
